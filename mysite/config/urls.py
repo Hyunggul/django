155942ca2adfd120b0d819e48591852c9b768364
lee-hyunggil <lhg094@gmail.com>
2023-08-23 18:05:57 +0900

    form ¸¸µé±â

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mysite project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/", include('accounts.urls')),
+    path('admin/', admin.site.urls),
+    path('pybo/', include('pybo.urls')),
 ]
